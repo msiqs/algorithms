a3485ae021a2a3064913b602157d83bc15549d67
Matheus Siqueira <msiqueiraa@proton.me>
2025-09-05 23:51:26 -0300

    Add age verification to lesson2.js

--- lesson2.js
+++ lesson2.js
@@ -1,1 +1,9 @@
-//
+//Resolução do execício 2 da lista.
+const prompt = require('prompt-sync')()
+let idade = parseInt(prompt('Digite sua idade: '))
+
+if(idade >= 18){
+    console.log('O usuário é maior de idade.')
+}else{
+    console.log('O usuário é menor de idade.')
+}
